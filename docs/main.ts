0c7430edd95d42549645209009884d15a62b9018
Josh Callebaut <josh.callebaut@gmail.com>
2019-04-18 21:24:47 -0700

    Add a spinner to indicate thread blocking

--- docs/main.ts
+++ docs/main.ts
@@ -1,6 +1,15 @@
 import "@babel/polyfill"; // IE11
 import 'whatwg-fetch'; // IE11
 
+const emojiRange = [128513, 128591];
+const emoji = Math.floor((emojiRange[1] - emojiRange[0]) * Math.random()) + emojiRange[0];
+
+const spinnerElement = <HTMLElement>document.querySelector("#spinner");
+spinnerElement.innerHTML = "&#" + emoji + ";";
+let spinner_rot = 0;
+const spinnerInterval = setInterval(function() {
+	spinnerElement.style.transform = `translateX(-50%) translateY(-50%) rotate(${spinner_rot++}deg)`
+}, 1);
 const progressElement = <HTMLElement>document.querySelector("#prog-inter");
 const progressText = <HTMLElement>document.querySelector("#prog-text");
 const progressDesc = <HTMLElement>document.querySelector("#prog-status");
@@ -13,7 +22,7 @@ let camera : THREE.PerspectiveCamera;
 let scene: THREE.Scene;
 let renderer: THREE.WebGLRenderer;
 let mesh: THREE.Mesh;
-let x: Float32Array;
+let vertexPoints: Float32Array;
 let bvh:BVH;
 
 function concatTypedArray(resultConstructor: Uint8ArrayConstructor, ...arrays:Uint8Array[]) {
@@ -26,7 +35,6 @@ function concatTypedArray(resultConstructor: Uint8ArrayConstructor, ...arrays:Ui
 		result.set(arr, offset);
 		offset += arr.length;
 	}
-	console.log("??")
 	return result;
 }
 
@@ -35,26 +43,26 @@ function concatTypedArray(resultConstructor: Uint8ArrayConstructor, ...arrays:Ui
 	const verts:Promise<Response> = fetch('./resources/models/dragon_vrip.f32verts');
 	const arrayBuffer:Promise<ArrayBuffer> = verts.then((response:Response):Promise<ArrayBuffer> => {
 		if(!response.body) return response.arrayBuffer(); // IE11
-		const bodyReader = (response as any).body.getReader();
-		const fileSize = (response as any).headers.get('content-length');
+		const bodyReader = response.body.getReader();
+		const contentLength = response.headers.get('content-length');
+		const fileSize = contentLength ? parseInt(contentLength, 10) : null;
 		return new Promise((res, rej) => {
-			let running:any = [];
+			let running:Uint8Array[] = [];
 			let runningResult = 0;
-			bodyReader.read().then(function read(result:any) {
-				if(result.done) {
-					return res(concatTypedArray(Uint8Array, ...running).buffer);
-				} else {
-					runningResult += result.value.length;
-					running.push(result.value);
+			bodyReader.read().then(function read(result:{value:Uint8Array, done:boolean}) {
+				if(result.done) return res(concatTypedArray(Uint8Array, ...running).buffer);
+				runningResult += result.value.length;
+				running.push(result.value);
+				if(fileSize) {
 					setProgress("Downloading model...", 0, 0.5, runningResult / fileSize, 1);
-					bodyReader.read().then(read);
 				}
-			})
+				bodyReader.read().then(read);
+			});
 		});
 	});
-	x = new Float32Array(await arrayBuffer);
-	bvh = await BVHBuilderAsync(x, undefined, {steps: 10}, ({trianglesLeafed}) => {
-		setProgress("Generating BVH...", 0.5, 1, trianglesLeafed, x.length / 9);
+	vertexPoints = new Float32Array(await arrayBuffer);
+	bvh = await BVHBuilderAsync(vertexPoints, undefined, {steps: 10}, ({trianglesLeafed}) => {
+		setProgress("Generating BVH...", 0.5, 1, trianglesLeafed, vertexPoints.length / 9);
 	});
 	setProgress("Done!", 0.5, 1, 1, 1);
 	initThree();
@@ -68,6 +76,8 @@ function setProgress(text: string, start:number, end:number, cur:number, max:num
 }
 
 function initThree() {
+	clearInterval(spinnerInterval);
+	spinnerElement.style.display = "none";
 	camera = new THREE.PerspectiveCamera(0.75, window.innerWidth / window.innerHeight, 1, 100);
 	camera.position.set(0, 10, 10);
 	camera.lookAt(0, 0.1, 0);
@@ -76,7 +86,7 @@ function initThree() {
 	scene.add( light1 );
 	scene.add( new THREE.AmbientLight( 0x444444 ) );
 	var geometry = new THREE.BufferGeometry();
-	geometry.addAttribute('position', new THREE.BufferAttribute(x, 3));
+	geometry.addAttribute('position', new THREE.BufferAttribute(vertexPoints, 3));
 	geometry.computeVertexNormals();
 	var material = new THREE.MeshStandardMaterial( { color: 0x00ff55 } );
 	mesh = new THREE.Mesh( geometry, material );
